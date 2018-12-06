53737db6c28b24cf3732d9577fc4e5efce76e764
deanmixer <deanmixer@users.noreply.github.com>
2018-12-05 23:43:59 -0500

    Adding sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,6 +30,8 @@
             wins++;
             document.querySelector("#score").innerHTML = wins;
             document.querySelector("#clue").innerHTML = "Press any key to play again!"; 
+            document.querySelector("#word").classList.add("correct");  
+            document.getElementById("bigWin").play(); 
         }
     }
 
@@ -40,7 +42,8 @@
             for(i = 0; i < wordEval.length; i++) {
                 document.querySelector("#word").innerHTML += wordEval[i] + " ";                   
                 };
-            document.querySelector("#word").classList.add("incorrect");    
+            document.querySelector("#word").classList.add("incorrect");  
+            document.getElementById("bigLose").play(); 
         }
     }
 
@@ -72,7 +75,7 @@
         correctGuesses = [];
         incorrectGuesses = [];
         updateHangman(); 
-        document.querySelector("#word").classList.remove("incorrect");  
+        document.querySelector("#word").classList.remove("incorrect", "correct");  
         document.querySelector("#remaining").innerHTML = guessRemain; 
         document.querySelector("#guessed").innerHTML = "None"; 
         document.querySelector("#clue").innerHTML = "Good luck!"; 
